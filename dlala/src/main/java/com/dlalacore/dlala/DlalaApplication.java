389a273097dc4346395b62d75591bc8da471fd55
zakaria.djellal <zakaria.djellal@racj.gouv.qc.ca>
2023-07-09 20:53:15 -0400

    integration de sprin boot

---
+++ dlala/src/main/java/com/dlalacore/dlala/DlalaApplication.java
@@ -0,0 +1,17 @@
+package com.dlalacore.dlala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@SpringBootApplication
+public class DlalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlalaApplication.class, args);
+	}
+
+}
---
+++ dlala/src/main/java/com/dlalacore/dlala/controller/UtilisateurController.java
@@ -0,0 +1,25 @@
+package com.dlalacore.dlala.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dlalacore.dlala.entities.Utilisateur;
+import com.dlalacore.dlala.services.UtilisateursService;
+
+@RestController
+@RequestMapping("/api/utilisateurs")
+public class UtilisateurController {
+	@Autowired
+	UtilisateursService userService;
+
+	@GetMapping("/utilisateursListe")
+	public List<Utilisateur> getUtilisateurs() {
+		return userService.getAll();
+
+	}
+
+}
